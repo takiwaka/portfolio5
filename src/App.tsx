007d20840a1e48a03fe07378fd4efccf0f6b68ec
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-09-08 14:47:51 +0900

    react typescript練習中

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
 import React from "react";
 import Load from './Load'
 
-
-interface Props {}
+interface Props { }
 
 interface State {
   message: string;
 }
 
-class App extends React.Component<Props,State> {
-  constructor(props:Props){
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
     super(props);
 
     this.state = {
-      message:"最初のメッセージ"
+      message: "最初のメッセージ"
     };
 
-this.handClick = this.handClick.bind(this);
-
-handClick(){
-  this.setState({
-    message:"ボタンが押されました"
-  });
-}
-
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick() {
+    this.setState({
+      message: "ボタンが押されました"
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
         <p>Hello world</p>
-        <Load num={"5"} />
+        <Sample num={"5"} />
 
         <p>{this.state.message}</p>
-        <button onclick={this.handClick}>ボタン</button>
+        <button onClick={this.handleClick}>ボタン</button>
       </React.Fragment>
     );
   }
