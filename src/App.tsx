6be08bd2bfdba24d9d68ab12039287563e001b9e
kawakita <hakusou.kokuyou@gmail.com>
2021-09-09 07:08:40 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,26 @@
-import React from "react";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-interface Props { }
-
-interface State {
-  message: string;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
-class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      message: "最初のメッセージ"
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState({
-      message: "ボタンが押されました"
-    });
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <p>Hello world</p>
-        <Sample num={"5"} />
-
-        <p>{this.state.message}</p>
-        <button onClick={this.handleClick}>ボタン</button>
-      </React.Fragment>
-    );
-  };
-};
 
 export default App;
