5c8c60989f876810785dbe4748acfa3f155b6f4a
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-09-14 14:01:41 +0900

    styled-Components化中

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import TaskList from "Components/Parts/TaskList";
 import TaskInput from "Components/Parts/TaskInput";
 import { Task } from "Types";
-import './Style/task.scss'
+import './Style/task.scss';;
+
 
 const initialState: Task[] = [
   {
--- src/Components/Parts/TaskInput.tsx
+++ src/Components/Parts/TaskInput.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import Styled from 'styled-Components';
 import { Task } from "Types";
-
+components
 type Props = {
   setTasks: React.Dispatch<React.SetStateAction<Task[]>>
   tasks: Task[]
@@ -26,19 +27,44 @@ const TaskInput= ({ setTasks, tasks }:Props) => {
 
   return (
     <div>
-      <div className="input-form">
-        <div className="inner">
-          <input
+      <InputForm>
+        <Inner className="inner">
+          <Input
             type="text"
             className="input"
             value={inputTitle}
             onChange={handleInputChange}
           />
           <button onClick={handleSubmit} className="btn is-primary">追加</button>
-        </div>
-      </div>
+        </Inner>
+      </InputForm>
     </div>
   )
 }
 
 export default TaskInput;
+
+const InputForm = styled.div`
+margin-bottom: 40px;
+background: #f9f3ee;
+padding: 40px 0;
+border-bottom: 1px solid #d7d2cd;
+`
+const Inner = styed.div`
+display: flex;
+position: relative;
+max-width: 700px;
+margin: 0 auto;
+`
+const Input = styed.input`
+width: 80%;
+font-size: 15px;
+outline: none;
+border: solid 3px #d7d2cd;
+padding: 10px;
+border-radius: 7px;
+margin-right: 10px;
+&:focus{
+  background: #f9f9f0;
+}
+`
