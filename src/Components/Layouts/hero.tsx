dcd7a1672027be968ded4a63b12e5175e101b717
kawakita <hakusou.kokuyou@gmail.com>
2021-09-17 07:43:59 +0900

    styled-media-query導入

--- src/Components/Layouts/hero.tsx
+++ src/Components/Layouts/hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import media from "styled-media-query";
 
 const Hero = () => {
   return (
@@ -32,6 +33,7 @@ const Content = styled.div`
   background-repeat: no-repeat;
   background-color: rgba(255, 255, 255, 0.8);
   background-blend-mode: lighten;
+
 `;
 
 const Heading = styled.h1`
@@ -40,13 +42,17 @@ const Heading = styled.h1`
   left: 50%;
   transform: translate(-50%, -50%);
   background-color: rgba(255, 255, 255, 0.8);
-
+  font-size: 2rem;
+  ${media.lessThan("small")`
+ font-size:1rem;
+  `}
 `;
 
 const Arrow = styled.div`
   position: absolute;
-  bottom: 0;
+  bottom: 10px;
   left: 50%;
   transform: translate(-50%);
   font-family: din1451alt;
+  font-size: 2rem;
 `;
